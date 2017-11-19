6973c7ba7eaa1d80146c0775043f2a0b7b715a02
Leo <leonhard.lerbs@hs-furtwangen.de>
2017-11-19 12:31:05 +0100

    First tweak for Doxygen test

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -2,9 +2,30 @@ package helloWorld;
 
 public class HelloWorld {
 
+
+	/**
+	 * Main function of helloWorld Project
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+		HelloWorld h = new HelloWorld();
+		h.test("this is Sparta");
 
+	}
+	
+	/**
+	 * Function will output a String to a console.
+	 * 
+	 * @param test 
+	 * Output for console
+	 */
+	public void test(String test) {
+
+
+		System.out.println(test);
 	}
 
 }
