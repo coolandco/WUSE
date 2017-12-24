f44a7b901330cb77ad3550a9725cd35f5f3913fc
leo <leonhard.lerbs@hs-furtwangen.de>
2017-12-24 17:13:09 +0100

    added "public" to A_distance

--- src/com/WUSE/AbstractUnits/A_speed.java
+++ src/com/WUSE/AbstractUnits/A_speed.java
@@ -4,7 +4,7 @@ import com.WUSE.Units.Unit;
 import com.WUSE.Units.UnitOperators;
 import com.WUSE.Time.A_Time;
 import com.WUSE.Units.BaseUnit;
-import com.WUSE.AbstractUnits.A_distance;
+import com.WUSE.Distance.A_distance;
 
 public class A_speed extends Unit<A_speed>{
 
--- src/com/WUSE/Distance/A_distance.java
+++ src/com/WUSE/Distance/A_distance.java
@@ -1,10 +1,11 @@
 package com.WUSE.Distance;
 
+
 import com.WUSE.AbstractUnits.A_speed;
 import com.WUSE.Time.A_Time;
 import com.WUSE.Units.BaseUnit;
 
-abstract class A_distance extends BaseUnit<A_distance>{
+public abstract class A_distance extends BaseUnit<A_distance>{
 	
 	
 	protected A_distance(float baseRate,float value) {
@@ -15,7 +16,7 @@ abstract class A_distance extends BaseUnit<A_distance>{
 	public A_speed dividedBy(A_Time time) {
 		
 		//returns a new Speed
-
+		
 		return new A_speed(this, time);
 	}	
 
